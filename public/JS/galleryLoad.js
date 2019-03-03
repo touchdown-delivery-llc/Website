a6bf0d435c7fcd8a82c0ce373472898213cd7dff
Darien Tsai <smartdragondt@gmail.com>
2019-03-02 21:59:51 -0800

    updated the media queries, footer, and gallery

--- public/JS/galleryLoad.js
+++ public/JS/galleryLoad.js
@@ -16,21 +16,25 @@ let path = "res/gallery/";
 let count = 0;    //for tracking the interval
 
 //appends each image into viewer, with a short delay each
-append = setInterval(
+setTimeout(
   function(){
+    setInterval(
+      function(){
 
-    //create and add image to view area
-    let source = path + JSON.stringify(image[count].name).substring(1, JSON.stringify(image[count].name).length - 1);
-    var newImg = document.createElement("img");
-    newImg.setAttribute("src", source);
-    newImg.setAttribute("class", "item");
-    view.appendChild(newImg);
-    count += 1;
+        //create and add image to view area
+        let source = path + JSON.stringify(image[count].name).substring(1, JSON.stringify(image[count].name).length - 1);
+        var newImg = document.createElement("img");
+        newImg.setAttribute("src", source);
+        newImg.setAttribute("class", "item");
+        view.appendChild(newImg);
+        count += 1;
 
-    //stops appending when reaching end of image collection
-    if( count >= image.length ){
-      clearInterval(append);
-    }
-  },
-  100
+        //stops appending when reaching end of image collection
+        if( count >= image.length ){
+          clearInterval(append);
+        }
+      },
+      15
+    )},
+  175
 );
--- public/JSON/galleryEntry.js
+++ public/JSON/galleryEntry.js
@@ -9,21 +9,31 @@
  */
 
  /* positions stored in format of:
-{
-  name: .jpg
-
-}
+{name: .jpg}
  */
 
  //stores the gallery iamge names as JSON objects
 var image = [
-  {
-    name: "temp.jpg"
-  },
-  {
-    name: "temp.jpg"
-  },
-  {
-    name: "temp.jpg"
-  }
+  {name: "TDD_8_BatteryCompartment.png"},
+  {name: "TDD_8_Bottom.png"},
+  {name: "TDD_8_Flight.png"},
+  {name: "TDD_8_FlightBottom.png"},
+  {name: "TDD_8_Frame.png"},
+  {name: "TDD_8_Landed.png"},
+  {name: "TDD_8_Landers.png"},
+  {name: "TDD_8_ProfileOpen.png"},
+  {name: "TDD_8_Rotor.png"},
+  {name: "TDD_8_Side.png"},
+  {name: "TDD_8_Tail.png"},
+  {name: "TDD_8_ThreeQuartersLanded.png"},
+  {name: "TDD_8_ThreeQuartersOpen.png"},
+  {name: "TDD_8_ThreeQuartersView.png"},
+  {name: "TDD_8_Top.png"},
+  {name: "TDD_8_TopFront.png"},
+  {name: "TDD_8_TopLanded.png"},
+  {name: "TDD_8-1_Battery.png"},
+  {name: "TDD_8_Charger.png"},
+  {name: "TDD_8_TopFront.png"},
+  {name: "TDD_8_ChargeSlot.png"},
+  {name: "TDD_8_ChargeVents.png"}
 ]
