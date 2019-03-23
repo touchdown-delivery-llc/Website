eae389813138af7d2760a4c88c5872bc833e32a3
Darien Tsai <smartdragondt@gmail.com>
2019-03-22 21:24:37 -0700

    fixed the infinite append loop for the gallery

--- public/JS/galleryLoad.js
+++ public/JS/galleryLoad.js
@@ -15,26 +15,29 @@ let path = "res/gallery/";
 
 let count = 0;    //for tracking the interval
 
+//timeout and interval
+var timeout = 175;
+var interval = 15;
+
 //appends each image into viewer, with a short delay each
 setTimeout(
   function(){
-    setInterval(
+    var append = setInterval(
       function(){
-
-        //create and add image to view area
-        let source = path + JSON.stringify(image[count].name).substring(1, JSON.stringify(image[count].name).length - 1);
-        var newImg = document.createElement("img");
-        newImg.setAttribute("src", source);
-        newImg.setAttribute("class", "item");
-        view.appendChild(newImg);
-        count += 1;
-
-        //stops appending when reaching end of image collection
-        if( count >= image.length ){
-          clearInterval(append);
+        
+        while(count < image.length){
+          //create and add image to view area
+          let source = path + JSON.stringify(image[count].name).substring(1, JSON.stringify(image[count].name).length - 1);
+          var newImg = document.createElement("img");
+          newImg.setAttribute("src", source);
+          newImg.setAttribute("class", "item");
+          view.appendChild(newImg);
+          count += 1;
         }
+        //stops appending when reaching end of image collection
+        clearInterval(append);
       },
-      15
+      interval
     )},
-  175
+  timeout
 );
