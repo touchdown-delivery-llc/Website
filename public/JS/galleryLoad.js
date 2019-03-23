4b63a48ff31a23c8cafe37a23cf0b9f4d03a0c8d
Darien Tsai <smartdragondt@gmail.com>
2019-03-22 21:38:10 -0700

    removed loading delays, gallery bug fixes

--- public/JS/galleryLoad.js
+++ public/JS/galleryLoad.js
@@ -13,31 +13,12 @@ let view = document.getElementById("presenter");
 //The path to the images area
 let path = "res/gallery/";
 
-let count = 0;    //for tracking the interval
-
-//timeout and interval
-var timeout = 175;
-var interval = 15;
-
-//appends each image into viewer, with a short delay each
-setTimeout(
-  function(){
-    var append = setInterval(
-      function(){
-        
-        while(count < image.length){
-          //create and add image to view area
-          let source = path + JSON.stringify(image[count].name).substring(1, JSON.stringify(image[count].name).length - 1);
-          var newImg = document.createElement("img");
-          newImg.setAttribute("src", source);
-          newImg.setAttribute("class", "item");
-          view.appendChild(newImg);
-          count += 1;
-        }
-        //stops appending when reaching end of image collection
-        clearInterval(append);
-      },
-      interval
-    )},
-  timeout
-);
+//appends each image into viewer
+for( var count = 0; count < image.length, count += 1){
+  //create and add image to view area
+  let source = path + JSON.stringify(image[count].name).substring(1, JSON.stringify(image[count].name).length - 1);
+  var newImg = document.createElement("img");
+  newImg.setAttribute("src", source);
+  newImg.setAttribute("class", "item");
+  view.appendChild(newImg);
+}
